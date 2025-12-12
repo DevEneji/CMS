9d690c06b6971d79f09946ba4fbb02e041b37ee3
DevEneji <josepheneji43@gmail.com>
2025-12-12 17:19:01 +0100

    Replaced frontend address in ALLOWED_HOSTS with backend address on render

--- media_site/core/settings.py
+++ media_site/core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--3)_33g5ro_m^$r=fp945ygz6h
 # SECURITY WARNING: don't run with debug turned on in production!
 # Use environment variable, default to True for safety
 DEBUG = 'True'
-ALLOWED_HOSTS = ['cms-react-frontend.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cms-xosi.onrender.com', 'localhost', '127.0.0.1']
 
 # Database
 DATABASES = {
